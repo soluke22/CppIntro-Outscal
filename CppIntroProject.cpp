e9fb59db3739be7cc6a89222fadd7aef0e8f3499
Solomon Lucas <soluke22@gmail.com>
2024-11-12 16:32:12 -0500

    added input and output

--- CppIntroProject.cpp
+++ CppIntroProject.cpp
@@ -9,5 +9,14 @@ int main() {
     "Don't worry, we'll find out together! \n" <<
     "Just know, that C++ isn't easy! So our challenges won't be easy either! \n" <<
     "But we can do it! \n";
+
+    
+    int playerHealth = 100;
+
+    cout <<"How much health do you have? \n";
+    cin >> playerHealth;
+    cout << "You have " << playerHealth << " health! \n" <<
+    "This won't stick around forever. \n";
+    
     return 0;
 }
