ceba7dc184b12180b9644c0787b788e76cd09867
Solomon Lucas <soluke22@gmail.com>
2024-11-12 16:16:48 -0500

    Add files via upload

---
+++ CppIntroProject.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    //used repl.it too much in college so I have to make github repos
+    cout << "Hello, World! I'm a C++ program! \n" << 
+    "I'm here to show you how the world works... \n" << 
+    "Maybe by punishment? Or by help! \n" << 
+    "Don't worry, we'll find out together! \n" <<
+    "Just know, that C++ isn't easy! So our challenges won't be easy either! \n" <<
+    "But we can do it! \n";
+    return 0;
+}
