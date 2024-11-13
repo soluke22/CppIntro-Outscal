4243a640ee3afbce2cee75fed672106f53eea51f
Solomon Lucas <soluke22@gmail.com>
2024-11-13 14:05:28 -0500

    added functions for healing, taking damage, and damage multipliers

--- CppIntroProject.cpp
+++ CppIntroProject.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
 using namespace std;
+
+int playerHealth = 100;
+int attack = 20;
+int defense = 10;
+int healing = 15;
+
+int TakeDamage(int damageToInflict) {
+    playerHealth -= damageToInflict;
+    if(playerHealth <= 0) {
+        playerHealth = 0;
+        cout << "You are dead! \n";
+    }
+    cout << "You took " << damageToInflict << " damage! \n";
+    cout << "Your health is now " << playerHealth << " points! \n";
+    return playerHealth;
+}
+
+int Heal(int amountToHeal) {
+    playerHealth += amountToHeal;
+    cout << "You healed " << amountToHeal << " health! \n" <<
+    "Your health is now " << playerHealth << " points! \n";
+    return playerHealth;
+}
+
+int DamageMultiplier(float multiplier) {
+    attack *= multiplier;
+    cout << "Your attack is now " << attack << " points! \n";
+    return attack;
+}
 int main() {
     //used repl.it too much in college so I have to make github repos
     cout << "Hello, World! I'm a C++ program! \n" << 
@@ -10,7 +39,6 @@ int main() {
     "Just know, that C++ isn't easy! So our challenges won't be easy either! \n" <<
     "But we can do it! \n";
 
-    int playerHealth = 100;
     int enemyHealth = 100;
     int playerLives = 3;
     string playerID = "1ABC23";
@@ -18,49 +46,11 @@ int main() {
     bool playerWon = false;
     char playerType = 'A';
 
-    int playerDamae = 20;
     int enemyDamage = 20;
 
-    bool isPlayerInRange = false;
-    bool isPlayerAttacking = false;
-    bool isPlayerDefending = false;
-
-    cout <<"How much health do you have? \n";
-    cin >> playerHealth;
-    cout << "You have " << playerHealth << " health! \n" <<
-    "This won't stick around forever. \n";
-    cout << "Though, what Type of player are you? \n" <<
-    "Only input A/B/C/D \n";
-    cin >> playerType;
-    if(playerType != 'A' && playerType != 'B' && playerType != 'C' && playerType != 'D') {
-        cout << "You are not a player! \n";
-    } else {
-        cout << "You are a player! \n";
-    }
-
-    cout << "Your enemy is attacking! \n" <<
-    "Are you in range? \n";
-    cin >> isPlayerInRange;
-    cout << "Are you going to attack? \n" <<
-    "Say true or false \n";
-    cin >> isPlayerAttacking;
-    if(!isPlayerAttacking) {
-        cout << "Are you going to defend? \n" <<
-        "Say true or false \n";
-        cin >> isPlayerDefending;
-    }
-
-    if (isPlayerInRange) {
-        if (isPlayerAttacking && playerHealth <= 20) {
-            cout << "Player should deliver a rage attack. \n";
-        } else if (isPlayerDefending) {
-            cout << "Enemy should hold. \n";
-        } else if (!isPlayerDefending && (playerHealth == 100 || playerHealth < 10)) {
-            cout << "Enemy should do a special ability attack. \n";
-        } else {
-            cout << "The enemy should attack! \n";
-        }
-    }
+    playerHealth = TakeDamage(enemyDamage);
+    playerHealth = Heal(healing);
+    attack = DamageMultiplier(1.5);
 
     return 0;
 }
